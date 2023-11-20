f2b5ac599f99d46a07646fc5555ffac05aee0ab5
Alina <121243874+gimmedemnomnoms@users.noreply.github.com>
2023-11-20 11:30:45 -0600

    wip

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -1,8 +1,10 @@
 import java.io.*;
 import java.net.*;
+import java.time.LocalDateTime;
 import java.util.*;
 
 public class UDPClient {
+
     public static void main(String args[]) throws Exception {
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
         DatagramSocket clientSocket = new DatagramSocket();
@@ -11,18 +13,49 @@ public class UDPClient {
         System.out.println(portNum);
         InetAddress IPAddress = InetAddress.getByName("127.0.0.1");
         boolean runAgain = true;
-        while (runAgain){byte[] sendData = new byte[1024];
+        String messageType;
+        System.out.println("Please enter your name: ");
+        String name = inFromUser.readLine().trim();
+        String joinMessage = "join|" + name;
+        byte[] joinData = joinMessage.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(joinData, joinData.length, IPAddress, 9876);
+        clientSocket.send(sendPacket);
+        byte[] receiveAck = new byte[1024];
+        DatagramPacket receivePacket = new DatagramPacket(receiveAck, receiveAck.length);
+        clientSocket.receive(receivePacket);
+        String ack = new String(receivePacket.getData()).trim();
+        String[] splitAck = ack.split("\\|");
+        System.out.println("Server response: " + splitAck[1]);
+        //System.out.println(name);
+        //messageType = "join|";
+        //String whatToSend = messageType + name;
+        //byte [] sending = whatToSend.getBytes();
+        //DatagramPacket sendPacket = new DatagramPacket(sending, sending.length, IPAddress, 9876);
+        //clientSocket.send(sendPacket);
+        //byte[] receiveAck = new byte[1024];
+        // DatagramPacket receivePacket = new DatagramPacket(receiveAck, receiveAck.length);
+        //clientSocket.receive(receivePacket);
+        //String ack = new String(receivePacket.getData());
+
+        //String [] splitAck = ack.split("\\|");
+        //splitAck[1] = splitAck[1].trim();
+        //System.out.println(splitAck[1]);
+
+
+        while (runAgain){
+            byte[] sendData = new byte[1024];
             byte[] receiveData = new byte[1024];
+            messageType = "math|";
             System.out.println("Enter a simple math problem in the format 'a + b'");
-
             String sentence = inFromUser.readLine();
-            sendData = sentence.getBytes();
-
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-            clientSocket.send(sendPacket);
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            String addheader = messageType + sentence;
+            sendData = addheader.getBytes();
+            DatagramPacket sendMathPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+            clientSocket.send(sendMathPacket);
+            receivePacket = new DatagramPacket(receiveData, receiveData.length);
             clientSocket.receive(receivePacket);
             String answerFormat = new String(receivePacket.getData());
+            answerFormat = answerFormat.trim();
             System.out.println("FROM SERVER: " + answerFormat);
 
             System.out.println("Would you like to ask another question? Enter 1 for yes or 2 for no");
@@ -32,8 +65,12 @@ public class UDPClient {
                 runAgain = false;
             }
         }
-
-
+        String endMessage = "end|";
+        byte[] endData = endMessage.getBytes();
+        DatagramPacket endPacket = new DatagramPacket(endData, endData.length, IPAddress,9876);
+        clientSocket.send(endPacket);
         clientSocket.close();
     }
+
+
 }
--- src/UDPServer.java
+++ src/UDPServer.java
@@ -1,66 +1,132 @@
 import java.io.*;
 import java.net.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class UDPServer {
-   public static void main(String args[]) throws Exception {
-       DatagramSocket serverSocket = new DatagramSocket(9876);
-       byte[] receiveData = new byte[1024];
-       byte[] sendData = new byte[1024];
+    static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    public static connection findClient(int port){
+        for (int i = 0; i < clients.size(); i++){
+            if (clients.get(i).getPortNumber() == port){
+                return clients.get(i);
+            }
+        }
+        return null;
+    }
+    public static class connection{
+        public int getPortNumber() {
+            return portNumber;
+        }
 
-       while(true) {
-           DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-           serverSocket.receive(receivePacket);
+        public String getUsername() {
+            return username;
+        }
 
-           String sentence = new String(receivePacket.getData());
-           ////////////// ADDED
-           InetAddress IPAddress = receivePacket.getAddress();
-           int port = receivePacket.getPort();
-           System.out.println("received: " + sentence + " from user " + port);
-           String[] splitsentence = sentence.split(" ");
-           splitsentence[0] = splitsentence[0].trim();
-           splitsentence[1] = splitsentence[1].trim();
-           splitsentence[2] = splitsentence[2].trim();
-           int num1 = Integer.parseInt(splitsentence[0]);
-           String operation = splitsentence[1];
-           int num2 = Integer.parseInt(splitsentence[2]);
+        int portNumber;
+        String username;
+        LocalDateTime startTime;
+        LocalDateTime endTime;
+        public connection(LocalDateTime start) {
+            this.startTime = start;
+        }
 
-           int result = 0;
+    }
+    private static List<connection> clients = new ArrayList<>();
 
-           switch(operation) {
-               case "+":
-                   result = num1 + num2;
-                   break;
-               case "-":
-                   result = num1 - num2;
-                   break;
-               case "*":
-                   result = num1 * num2;
-                   break;
-               case "/":
-                   result = num1/num2;
-                   break;
-               case "%":
-                   result = num1 % num2;
-                   break;
-               default:
-                   System.out.println("invalid");
-                   break;
-           }
-           String answer = Integer.toString(result);
+    public static void main(String args[]) throws Exception {
+        DatagramSocket serverSocket = new DatagramSocket(9876);
+        byte[] receiveData = new byte[1024];
+        byte[] sendData = new byte[1024];
+        String user;
 
-////////////////////
+        while(true) {
+            System.out.println(LocalDateTime.now());
+            DatagramPacket receiveMathPacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receiveMathPacket);
+            InetAddress IPAddress = receiveMathPacket.getAddress();
+            int port = receiveMathPacket.getPort();
+            String sentence = new String(receiveMathPacket.getData());
 
+            String [] splitType = sentence.split("\\|");
+            String messageType = splitType[0];
+            messageType = messageType.trim();
+            //System.out.println(messageType);
+            String replyMessage = null;
+            switch (messageType){
+                case "join":
+                    connection newClient = new connection(LocalDateTime.now());
+                    //clients.add(new connection(LocalDateTime.now()));
+                    user = splitType[1];
+                    user = user.trim();
+                    newClient.username = user;
+                    newClient.portNumber = port;
+                    clients.add(newClient);
+                    System.out.println(user + " has joined \t" + LocalDateTime.now().format(formatter));
+                    replyMessage = messageType + "|" + "Welcome, " + user;
+                    break;
+                case "math":
+                    String operation;
+                    connection currentUser = findClient(port);
+                    user = currentUser.getUsername();
+                    try{
+                        //System.out.println(LocalDateTime.now());
+                        // System.out.println("in the try");
+                        String [] splitMath = splitType[1].split(" ");
+                        // System.out.println("did the spliteroony");
+                        splitMath[0] = splitMath[0].trim();
+                        // System.out.println("trim 0: " + splitMath[0]);
+                        splitMath[1] = splitMath[1].trim();
+                        // System.out.println("trim 1: " + splitMath[1]);
+                        splitMath[2] = splitMath[2].trim();
+                        // System.out.println("trim 2: " + splitMath[2]);
+                        int num1 = Integer.parseInt(splitMath[0]);
+                        //  System.out.println("parsed num1");
+                        operation = splitMath[1];
+                        // System.out.println("set op");
+                        int num2 = Integer.parseInt(splitMath[2]);
+                        // System.out.println("parsed num2");
+                        int result = 0;
+                        // System.out.println("into the switch");
 
-           //String caps = sentence.toUpperCase();
-           sendData = answer.getBytes(); //modded
-
-           DatagramPacket sendpacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-           System.out.println("sending " + answer + " to port num " + port);
-
-           serverSocket.send(sendpacket);
-
-       }
-   }
-
-
+                        switch (operation) {
+                            case "+":
+                                result = num1 + num2;
+                                break;
+                            case "-":
+                                result = num1 - num2;
+                                break;
+                            case "*":
+                                result = num1 * num2;
+                                break;
+                            case "/":
+                                result = num1 / num2;
+                                break;
+                            case "%":
+                                result = num1 % num2;
+                                break;
+                            default:
+                                throw new Exception("You done goofed");
+                        }
+                        System.out.println("From: " + user + "\n" + num1 + operation + num2 + "\t" + LocalDateTime.now().format(formatter));
+                        String answer = Integer.toString(result);
+                        replyMessage = num1 + operation + num2 + "=" + answer;
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                case "end":
+                    replyMessage = messageType + "|" + "terminating session";
+                    currentUser = findClient(port);
+                    System.out.println("ending session with " + currentUser.username + "\t" + LocalDateTime.now().format(formatter));
+                    currentUser.endTime = LocalDateTime.now();
+            }
+            sendData = replyMessage.getBytes();
+            DatagramPacket sendpacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            //System.out.println("sending " + answer + " to port num " + port);
+            serverSocket.send(sendpacket);
+        }
+    }
 }
